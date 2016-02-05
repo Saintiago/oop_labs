848b9e8bd1d6d5bfd7b0beed15c3a0849a174b64
Saintiago <techpriest2007@yandex.ru>
2016-02-05 15:07:35 +0300

    Заменил while на do-while. Добавил возврат кодов успешности выполнения программы. Перенес try-catch внутрь цикла.

--- lab6/task01/task01.cpp
+++ lab6/task01/task01.cpp
@@ -7,23 +7,29 @@
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
-try
 {
-	string again = "y";
-	while (again == "y")
+	try
 	{
-		cout << "Please, enter three numbers for triangle sides." << endl;
+		string again;
 		double side1, side2, side3;
-		cin >> side1 >> side2 >> side3;
-		CTriangle triangle(side1, side2, side3);
-		cout << "Given triangle perimeter is " << triangle.GetPerimeter() << endl;
-		cout << "And his area is " << triangle.GetArea() << endl;
-		cout << "Want to try another triangle? (press 'y' to continue)" << endl;
-		cin >> again;
+		do
+		{
+			cout << "Please, enter three numbers for triangle sides." << endl;
+			cin >> side1 >> side2 >> side3;
+			CTriangle triangle(side1, side2, side3);
+			cout << "Given triangle perimeter is " << triangle.GetPerimeter() << endl;
+			cout << "And his area is " << triangle.GetArea() << endl;
+			cout << "Want to try another triangle? (press 'y' to continue)" << endl;
+			cin >> again;
+		} 
+		while (again == "y");
+
+		return 0;
 	}
-}
-catch (exception const & e)
-{
-	cout << "Oops! We caught an exception!" << endl;
-	cout << "And it says: \"" << e.what() << "\"" << endl;
+	catch (exception const & e)
+	{
+		cout << "Oops! We caught an exception!" << endl;
+		cout << "And it says: \"" << e.what() << "\"" << endl;
+		return 1;
+	}
 }
